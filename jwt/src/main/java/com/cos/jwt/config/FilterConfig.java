c57d5aeebe6a4b25322340e8d87e969241bee064
eunji-97 <eundev.ji@gmail.com>
2025-08-04 21:27:05 +0900

    22강 jwt 필터등록테스트

---
+++ jwt/src/main/java/com/cos/jwt/config/FilterConfig.java
@@ -0,0 +1,30 @@
+package com.cos.jwt.config;
+
+import com.cos.jwt.filter.MyFilter1;
+import com.cos.jwt.filter.MyFilter2;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+    //필터를 시큐리티에 걸지 않아도 됨, 순서는 시큐리티 필터 > 여기 필터 순서대로
+    @Bean
+    public FilterRegistrationBean<MyFilter1> filter1() {
+        FilterRegistrationBean<MyFilter1> bean = new FilterRegistrationBean<>(new MyFilter1());
+        bean.addUrlPatterns("/*");
+        bean.setOrder(0); //가장 먼저 실행됨
+        return bean;
+
+    }
+
+    @Bean
+    public FilterRegistrationBean<MyFilter2> filter2() {
+        FilterRegistrationBean<MyFilter2> bean = new FilterRegistrationBean<>(new MyFilter2());
+        bean.addUrlPatterns("/*");
+        bean.setOrder(1);
+        return bean;
+
+    }
+}
--- jwt/src/main/java/com/cos/jwt/config/SecurityConfig.java
+++ jwt/src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.cos.jwt.config;
 
+import com.cos.jwt.filter.MyFilter1;
+import com.cos.jwt.filter.MyFilter3;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
@@ -26,6 +29,8 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) //세션을 사용하지 않겠다
                 )
                 .addFilter(corsFilter) //시큐리티 필터에 등록
+//                .addFilter(myFilter1) //그냥 필터는 오류남 Consider using addFilterBefore or addFilterAfter instead.
+                .addFilterBefore(new MyFilter3(), BasicAuthenticationFilter.class)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/v1/user/**").hasAnyAuthority("USER", "ADMIN", "MANAGER")
                         .requestMatchers("/api/v1/manager/**").hasAuthority("MANAGER")
@@ -33,7 +38,8 @@ public class SecurityConfig {
                         .anyRequest().permitAll()
                 )
                 .formLogin(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable);
+                .httpBasic(AbstractHttpConfigurer::disable); //
+        //basic 방식 : ID, PW를 들고 요청 // bearer 방식 : token을 들고 요청
 
 
         return http.build();
